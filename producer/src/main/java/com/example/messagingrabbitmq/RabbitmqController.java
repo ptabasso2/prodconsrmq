52d710e355a93b574a3dc4d1086300f4de14ced3
ptabasso2 <37804130+ptabasso2@users.noreply.github.com>
2020-04-19 15:13:39 +0200

    Update RabbitmqController.java

--- producer/src/main/java/com/example/messagingrabbitmq/RabbitmqController.java
+++ producer/src/main/java/com/example/messagingrabbitmq/RabbitmqController.java
@@ -1,11 +1,5 @@
 package com.example.messagingrabbitmq;
 
-import datadog.trace.api.DDTags;
-import io.opentracing.Scope;
-import io.opentracing.ScopeManager;
-import io.opentracing.Span;
-import io.opentracing.Tracer;
-import io.opentracing.contrib.spring.rabbitmq.RabbitMqSendTracingHelper;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,30 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RabbitmqController {
 
-
     @Autowired
     RabbitTemplate rabbitTemplate;
 
-    @Autowired
-    Tracer tracer;
 
     @RequestMapping("/test")
     public String index() throws InterruptedException {
         System.out.println("Sending message from controller...");
         //rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
-        ScopeManager sm = tracer.scopeManager();
-        Tracer.SpanBuilder tb = tracer.buildSpan("servlet.request");
-
-        Span span = tb.start();
-        try(Scope scope = sm.activate(span)) {
-            span.setTag(DDTags.SERVICE_NAME, "springrabbitmqprod");
-            span.setTag(DDTags.RESOURCE_NAME, "GET /test");
-            span.setTag(DDTags.SPAN_TYPE, "web");
-            publishToRabbitMQ();
-            Thread.sleep(20);
-            span.finish();
-        }
-
+        publishToRabbitMQ();
         return "\ntest";
     }
 
