4fcb140235efc0379452e156d53af3dc9add7f3e
ptabasso2 <37804130+ptabasso2@users.noreply.github.com>
2020-04-19 15:19:35 +0200

    Update Receiver.java

--- consumer/src/main/java/com/example/messagingrabbitmq/Receiver.java
+++ consumer/src/main/java/com/example/messagingrabbitmq/Receiver.java
@@ -2,10 +2,6 @@ package com.example.messagingrabbitmq;
 
 import java.util.concurrent.CountDownLatch;
 
-import datadog.trace.api.DDTags;
-import io.opentracing.Scope;
-import io.opentracing.Span;
-import io.opentracing.Tracer;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -21,29 +17,15 @@ public class Receiver {
 	@Autowired
 	RestTemplate restTemplate;
 
-	@Autowired
-	Tracer tracer;
-
 	@RabbitListener(queues = "${consumer.rabbitmq.queue}")
 	public void receiveMessage(String message) {
 		System.out.println("Received <" + message + ">");
 		/*String result = restTemplate.getForObject("https://www.google.fr", String.class);
 		System.out.println(result);*/
 
-		Span resultingspan = tracer.activeSpan();
-
-		Tracer.SpanBuilder httpspan = tracer.buildSpan("hey google").asChildOf(resultingspan);
-		Span childspan = httpspan.start();
-
-		try(Scope scope = tracer.activateSpan(childspan)){
-			childspan.setTag(DDTags.RESOURCE_NAME, "GET /");
-			childspan.setTag(DDTags.SPAN_TYPE, "web");
-			childspan.setTag(DDTags.SERVICE_NAME, "Google");
-			httpRequest();
-			System.out.println("Hello google");
-			childspan.finish();
-		}
-
+		httpRequest();
+		System.out.println("Hello google");
+		
 		latch.countDown();
 	}
 
